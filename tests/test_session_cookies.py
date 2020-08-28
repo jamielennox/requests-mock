1a3f73a147d34ce86c6e2e60bdd98afbb9aee624
Simon Garrelou <simon.garrelou@airbus.com>
2020-08-28 11:31:52 +0200

    Rename test variables

--- tests/test_session_cookies.py
+++ tests/test_session_cookies.py
@@ -6,11 +6,11 @@ from . import base
 
 class SessionCookiesTest(base.TestCase):
     def test_cookie(self):
-        with requests_mock.Mocker() as m:
-            m.get("mock://test.site", text="hello", cookies={"name": "value"})
+        with requests_mock.Mocker() as mocker:
+            mocker.get("mock://test.site", text="hello", cookies={"name": "value"})
 
-            with requests.Session() as s:
-                r = s.get("mock://test.site")
+            with requests.Session() as session:
+                resp = session.get("mock://test.site")
 
-                self.assertEquals(r.cookies.get("name"), "value")
-                self.assertEquals(s.cookies.get("name"), "value")
+                self.assertEquals(resp.cookies.get("name"), "value")
+                self.assertEquals(session.cookies.get("name"), "value")
