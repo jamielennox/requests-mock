1489606623758f3aaf0e56b2c76f8679aa28a8b0
Jason R. Coombs <jaraco@jaraco.com>
2023-01-05 15:42:25 -0500

    Expand test to include two cookies.

--- tests/test_session_cookies.py
+++ tests/test_session_cookies.py
@@ -7,10 +7,13 @@ from . import base
 class SessionCookiesTest(base.TestCase):
     def test_cookie(self):
         with requests_mock.Mocker() as mocker:
-            mocker.get("mock://test.site", text="hello", cookies={"name": "value"})
+            cookies = {"name": "value", "name2": "value2"}
+            mocker.get("mock://test.site", text="hello", cookies=cookies)
 
             with requests.Session() as session:
                 resp = session.get("mock://test.site")
 
                 self.assertEquals(resp.cookies.get("name"), "value")
                 self.assertEquals(session.cookies.get("name"), "value")
+
+                self.assertEquals(session.cookies.get("name2"), "value2")
